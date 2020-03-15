a9e1ff14a96b26a0ff0f192ebd67475f718b1dab
Mogen <eppcmogen@gmail.com>
2020-03-15 16:01:05 -0400

    menu and components

---
+++ app.js
@@ -0,0 +1,11 @@
+function toggleHeader() {
+    let links = document.getElementById("links");
+    let menu_title = document.getElementById("menu-link-text");
+
+    // Toggle between none and block
+    links.style.display = (links.style.display ===  "none") ? "block" : "none";
+    menu_title.innerHTML = (links.style.display==="block") ? "Close" : "Menu";
+}
+
+// Toggle drop down when header is clicked
+document.getElementById("header").addEventListener("click", toggleHeader);
